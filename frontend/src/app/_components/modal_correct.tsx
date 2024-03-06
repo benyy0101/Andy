9da9812f239b6e32ae2e6d323ec5d925cad528c2
sueminpark <breathin_suemin@naver.com>
2024-03-06 17:59:30 +0900

    S10P22A102-82 FEAT: create  Quiz1 page, component - confetti issue

---
+++ frontend/src/app/_components/modal_correct.tsx
@@ -0,0 +1,69 @@
+'use client'
+import React, { useState } from 'react';
+import { TConductorInstance } from "react-canvas-confetti/dist/types";
+import Fireworks from "react-canvas-confetti/dist/presets/fireworks";
+import tw from 'tailwind-styled-components';
+
+import Image from 'next/image';
+import CorrectImage from '@/app/asset/_img/correct.png';
+
+
+interface CorrectModalProps {
+  isOpen: boolean;
+  onClose: () => void;
+}
+
+const CorrectModal: React.FC<CorrectModalProps> = ({ isOpen, onClose }) => {
+  const [conductor, setConductor] = useState<TConductorInstance>();
+
+  const onInit = ({ conductor }: { conductor: TConductorInstance }) => {
+    setConductor(conductor);
+    conductor.shoot();
+  };
+
+  // 의존성 문제 
+  // Maximum update depth exceeded.
+
+  const CorrectBackground = tw.div`
+  fixed 
+  top-0 
+  left-0 
+  w-full 
+  h-full 
+  flex items-center 
+  justify-center 
+  bg-black
+  bg-opacity-60
+  `;
+
+  const CorrectContent = tw.div`
+  bg-yellow
+  p-8
+  rounded-lg
+  text-center
+  w-96
+  `;
+
+  return (
+    <>
+      {isOpen && (
+        <CorrectBackground>
+          <Fireworks onInit={onInit} />
+          <CorrectContent>
+            {/* 정답 이미지 */}
+            <div className="mb-10 flex items-center justify-center">
+              <Image src={CorrectImage} alt="정답" width="150" height="150" />
+            </div>
+            <>
+            </>
+            <br />
+            {/* 멘트 */}
+            <button onClick={onClose} className="font-bold text-xl">정답입니다 !</button>
+          </CorrectContent>
+        </CorrectBackground>
+      )}
+    </>
+  );
+};
+
+export default CorrectModal;
