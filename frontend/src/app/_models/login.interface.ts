9cb8f0ed384dd000787953b273d62c81e0ec5822
benyy0101 <benyy0101@gmail.com>
2024-03-13 14:27:35 +0900

    S10P22A102-105 feat: 프로필 훅 및 이미지 훅 작성 Preparing commit message

--- frontend/src/app/_models/login.interface.ts
+++ frontend/src/app/_models/login.interface.ts
@@ -1,4 +1,5 @@
 import { gender } from "./enums";
+import { BabyProfile } from "./profile.interface";
 
 export interface KakaoAuth {
     code: string;
@@ -19,41 +20,7 @@ export interface LoginResponse{
     profiles: BabyProfile[],
 }
 
-
-
 export interface KakaoLogoutRequest {
     logout_redirect_uri: string,
 }
 
-export interface BabyProfile {
-    child_seq: string;
-    child_name: string;
-    child_picture: string;
-}
-
-export interface CreateBabyProfileRequest {
-    kakao_id:string;
-    child_name: string,
-    child_birthday:string,
-    child_gender: gender,
-    child_picture: string | null,
-}
-
-export interface CreateBabyProfileResponse{
-	child_seq:number,
-	child_name:string,
-	child_picture: string
-}
-
-export interface RemoveBabyProfileRequest {
-    child_seq: string;
-}
-
-export interface ProfileState extends BabyProfile{
-    kakao_name: string,
-}
-
-export interface ProfileActions {
-    setProfileInfo: (profile: ProfileState) => void;
-    removeProfileInfo: () => void;
-}
--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -20,7 +20,7 @@ export default function RootLayout({
   return (
     <html lang="en">
         <body className={inter.className}>
-          <ReactQueryProviders>{children}</ReactQueryProviders>
+        {children}
         </body>
     </html>
   );
