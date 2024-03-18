cd1fdf8f8f781921472ad25ff0b979f3ee0aa367
benyy0101 <benyy0101@gmail.com>
2024-03-18 22:01:04 +0900

    S10P22A102-142 fix: 중요 환경변수 숨김

--- frontend/src/app/(route)/main/_components/KakaoLogin.tsx
+++ frontend/src/app/(route)/main/_components/KakaoLogin.tsx
@@ -1,32 +1,37 @@
-'use client'
+"use client";
 import tw from "tailwind-styled-components";
-import kakao_logo from "../../../asset/_img/kakao_logo.png"
+import kakao_logo from "../../../asset/_img/kakao_logo.png";
 import Image from "next/image";
 
 export const KakaoLogin = () => {
-    const KAKAO_REST_API_KEY = process.env.NEXT_PUBLIC_API_KEY;
-    const KAKAO_REDIRECT_URL = "http://localhost:3000/login/kakao"
-    const KAKAO_AUTH_URL = `https://kauth.kakao.com/oauth/authorize?client_id=${KAKAO_REST_API_KEY}&redirect_uri=${KAKAO_REDIRECT_URL}&response_type=code`;
+  const KAKAO_REST_API_KEY = process.env.NEXT_PUBLIC_API_KEY;
+  const KAKAO_REDIRECT_URL = process.env.NEXT_PUBLIC_KAKAO_REDIRECT_URL;
+  const KAKAO_AUTH_URL = `https://kauth.kakao.com/oauth/authorize?client_id=${KAKAO_REST_API_KEY}&redirect_uri=${KAKAO_REDIRECT_URL}&response_type=code`;
 
-    const handleLogin = () => {
-        window.location.href = KAKAO_AUTH_URL
-    }
+  const handleLogin = () => {
+    window.location.href = KAKAO_AUTH_URL;
+  };
 
-    return (
-        <div>
-            <LoginBtn onClick={handleLogin}>
-                <Symbol>
-                    <Image src={kakao_logo.src}
-                    alt="kakao logo"
-                    height={21}
-                    width={24}
-                    style={{filter: "invert(0%) sepia(1%) saturate(4%) hue-rotate(25deg) brightness(98%) contrast(101%)" }}/>
-                </Symbol>
-                <Label>카카오 로그인</Label>
-            </LoginBtn>
-        </div>
-    )
-}
+  return (
+    <div>
+      <LoginBtn onClick={handleLogin}>
+        <Symbol>
+          <Image
+            src={kakao_logo.src}
+            alt="kakao logo"
+            height={21}
+            width={24}
+            style={{
+              filter:
+                "invert(0%) sepia(1%) saturate(4%) hue-rotate(25deg) brightness(98%) contrast(101%)",
+            }}
+          />
+        </Symbol>
+        <Label>카카오 로그인</Label>
+      </LoginBtn>
+    </div>
+  );
+};
 
 const LoginBtn = tw.button`
 w-[230px]
@@ -44,12 +49,12 @@ justify-center
 items-center
 border-[3px]
 border-[white]
-`
+`;
 
 const Symbol = tw.div`
 color-[#000000]
 w-[18%]
-`
+`;
 
 const Label = tw.div`
 text-black
@@ -57,4 +62,4 @@ opacity-[85%]
 w-[65%]
 font-bold
 text-[16px]
-`
+`;
--- frontend/src/app/api/http-commons.ts
+++ frontend/src/app/api/http-commons.ts
@@ -1,63 +1,63 @@
-import axios, { AxiosInstance, AxiosRequestConfig, AxiosResponse } from 'axios';
+import axios, { AxiosInstance, AxiosRequestConfig, AxiosResponse } from "axios";
 
 const serverUrl = process.env.SERVER_URL || "";
+const loginUrl = process.env.NEXT_PUBLIC_LOGIN_URL || "";
 const imageBaseUrl = process.env.IMAGE_BASE_URL || "";
 const localDev = process.env.LOCAL_DEV || false;
 
 const saveToken = (response: AxiosResponse) => {
-    if(response.data.jwtToken !== null){
-        localStorage.setItem('jwtToken',response.data.jwtToken);
-    }
-}
+  if (response.data.jwtToken !== null) {
+    localStorage.setItem("jwtToken", response.data.jwtToken);
+  }
+};
 
 const loadToken = (config: AxiosRequestConfig) => {
-    const token = localStorage.getItem('jwtToken');
-    if(token && config.headers){
-        return Object.assign(config.headers, {Authorization: `Bearer ${token}`});
-    }
-    return null;    
-}
+  const token = localStorage.getItem("jwtToken");
+  if (token && config.headers) {
+    return Object.assign(config.headers, { Authorization: `Bearer ${token}` });
+  }
+  return null;
+};
 
 export const localAxios = axios.create({
-    baseURL: serverUrl,
-    headers: {
-        "Content-type": "application/json",
-        withCredentials: true,
-    }
+  baseURL: serverUrl,
+  headers: {
+    "Content-type": "application/json",
+    withCredentials: true,
+  },
 });
 
 export const kakaoAxios = axios.create({
-    baseURL: serverUrl,
-    headers: {
-        "Content-type": "application/json",
-        withCredentials: true,
-    }
+  baseURL: loginUrl,
+  headers: {
+    "Content-type": "application/json",
+    withCredentials: true,
+  },
 });
 
 export const imageAxios: AxiosInstance = axios.create({
-    baseURL: imageBaseUrl,
-    timeout: 10000,
-    headers: {
-      "Content-Type": "multipart/form-data",
-    },
-    withCredentials: true,
-  });
-
-localAxios.interceptors.response.use((response)=>{
-    saveToken(response);
-    
-    return response;
-},
-(error)=>{
-    // eslint-disable-next-line no-console
-    if(localDev) console.error(error);
-    return error;
+  baseURL: imageBaseUrl,
+  timeout: 10000,
+  headers: {
+    "Content-Type": "multipart/form-data",
+  },
+  withCredentials: true,
 });
 
-localAxios.interceptors.request.use((config) => {
-    loadToken(config);
-    return config;
-})
+localAxios.interceptors.response.use(
+  (response) => {
+    saveToken(response);
 
-    
-    
+    return response;
+  },
+  (error) => {
+    // eslint-disable-next-line no-console
+    if (localDev) console.error(error);
+    return error;
+  },
+);
+
+localAxios.interceptors.request.use((config) => {
+  loadToken(config);
+  return config;
+});
