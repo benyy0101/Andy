8d7741d218aecc128306801f67de6fc0e0199c94
benyy0101 <benyy0101@gmail.com>
2024-03-18 10:33:41 +0900

    S10P22A102-141 feat: eslint에 맞게 수정중 - feat-front 머지 대기 Preparing commit message

--- frontend/src/app/_components/logo/Logo.tsx
+++ frontend/src/app/_components/logo/Logo.tsx
@@ -1,11 +1,9 @@
 'use client'
-import tw from "tailwind-styled-components";
+
 import { useRouter } from "next/navigation";
+import {Wrapper, Title} from './styles/Logo.styled';
 
-import Image from "next/image";
-import logo from "../../asset/_img/Andylogo.png";
-
-export const Logo = () => {
+function Logo() {
     const router = useRouter();
 
     const routetoHome = () => {
@@ -19,6 +17,8 @@ export const Logo = () => {
     )
 }
 
+export default Logo;
+
 // const Wrapper = tw.div`
 // flex
 // justify-center
@@ -27,17 +27,3 @@ export const Logo = () => {
 // top-[40px]
 // `
 
-const Wrapper = tw.div`
-absolute
-top-[40px]
-left-0
-right-0
-flex
-justify-center
-`
-
-const Title = tw.div`
-font-bold
-text-3xl
-cursor-pointer
-`
