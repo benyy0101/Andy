9da0f0730a1006cb709e034f3350ce7554a6ea66
benyy0101 <benyy0101@gmail.com>
2024-03-18 11:03:46 +0900

    S10P22A102-141 feat: 카메라 컴포넌트 병합중 Preparing commit message

--- frontend/src/app/(route)/quiz1/_components/camera.tsx
+++ frontend/src/app/(route)/quiz1/_components/camera.tsx
@@ -1,8 +1,14 @@
-import React from "react";
+'use client';
+
+import React, { useEffect } from "react";
 import { CameraBox } from "./styles/Camera.styled";
+import { PhotoBooth } from "./photobooth";
 
 function Camera() {
-    
+    useEffect(() => {
+        PhotoBooth.init();
+    },[]);
+
     return (
         <CameraBox>카메라</CameraBox>
     );
