9cd28fba2e8fd3a98116c7ae80852e751f9054cf
DH <alexander930828@gmail.com>
2024-03-20 09:47:45 +0900

    Feat: Modify all logics

--- ai/Server.py
+++ ai/Server.py
@@ -1,19 +1,23 @@
 from fastapi import FastAPI, HTTPException
 from fastapi.responses import FileResponse
-
-from main import print_hi
+from pydantic import BaseModel
+from ObjectiDetection import ObjectDetection
 
 app = FastAPI()
 
-@app.get("/")
-async def get_image():
-    # 이미지 파일 경로 설정
-    image_path = "C:/Users/SSAFY/Desktop/image/test.jpg"  # 이미지 파일 경로를 실제 이미지 파일의 경로로 변경하세요.
+class ImageRequest(BaseModel):
+    image_path: str
+    answer: str
 
+
+@app.post("/")
+async def get_image(request: ImageRequest):
     try:
-        # 이미지 파일을 응답으로 반환
-        
-        return FileResponse(image_path, media_type="image/jpeg")
+        # 이미지 파일 경로와 answer 받기
+        File = FileResponse(request.image_path, media_type="image/jpeg")
+        answer = request.answer
+        return ObjectDetection(File, answer)
+
     except Exception as e:
-        # 예외 처리 열심히
+        # 예외 처리
         raise HTTPException(status_code=404, detail="Image not found")
