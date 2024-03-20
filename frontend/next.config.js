42c77c805919fc70f7d49aaa3cdba05a0d7ca0b5
benyy0101 <benyy0101@gmail.com>
2024-03-20 14:37:06 +0900

    S10P22A102-144 feat: swap useQuery to SuspenseQuery

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,4 +1,3 @@
-
 /** @type {import('next').NextConfig} */
 const withPWA = require('@ducanh2912/next-pwa').default({
   dest: "public",
--- frontend/src/app/(route)/profile_list/page.tsx
+++ frontend/src/app/(route)/profile_list/page.tsx
@@ -1,18 +1,22 @@
-import Logo from "../../_components/logo/Logo"
-import Profile from "./_components/profile"
-import ProfileAdd from "./_components/profile_add"
+import { useProfileList } from "@/app/hooks/useProfile";
+import Logo from "../../_components/logo/Logo";
+import Profile from "./_components/profile";
+import ProfileAdd from "./_components/profile_add";
 // import { Tutorial_Btn } from "../../_components/tutorial_btn/tutorial_btn"
-import { Wrapper, Profiles } from "./styles/Page.styled"
+import { Wrapper, Profiles } from "./styles/Page.styled";
 
 export default function ProfileList() {
-    return (
-        <Wrapper>
-            <Logo />
-            <Profiles>
-                <Profile />
-                <ProfileAdd />
-            </Profiles>
-            {/* <Tutorial_Btn /> */}
-        </Wrapper>
-    )
-};
+  const { data } = useProfileList();
+  // eslint-disable-next-line no-console
+  console.log(data);
+  return (
+    <Wrapper>
+      <Logo />
+      <Profiles>
+        <Profile />
+        <ProfileAdd />
+      </Profiles>
+      {/* <Tutorial_Btn /> */}
+    </Wrapper>
+  );
+}
