c91bab5f5e20973c36e10c5cd0419b3dc8097885
benyy0101 <benyy0101@gmail.com>
2024-03-20 16:03:52 +0900

    S10P22A102-144 feat: fetchting profile list ready to merge

--- frontend/src/app/(route)/profile_list/page.tsx
+++ frontend/src/app/(route)/profile_list/page.tsx
@@ -1,14 +1,14 @@
-"use client";
-
 import TutorialBtn from "@/app/_components/tutorial_btn/tutorialBtn";
 import Logo from "../../_components/logo/Logo";
 import Profile from "./_components/profile";
 import ProfileAdd from "./_components/profile_add";
 import { Wrapper, Profiles } from "./styles/Page.styled";
+import ProfileBox from "./_components/ProfileBox";
 
 export default function ProfileList() {
   return (
     <Wrapper>
+      <ProfileBox />
       <Logo />
       <Profiles>
         <Profile />
