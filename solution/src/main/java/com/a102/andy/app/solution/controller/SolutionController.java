ec8a6b2284b5f233bbd20f133240ed4153823942
Team207 <luice0310@naver.com>
2024-03-25 10:46:13 +0900

    refactor method private -> public

--- solution/src/main/java/com/a102/andy/app/solution/controller/SolutionController.java
+++ solution/src/main/java/com/a102/andy/app/solution/controller/SolutionController.java
@@ -18,21 +18,21 @@ public class SolutionController {
 
     //카테고리 조회
     @GetMapping("/category")
-    private ResponseEntity<List<CategoriesResponseDto>> readCategoryAll(){
+    public ResponseEntity<List<CategoriesResponseDto>> readCategoryAll(){
        return ResponseEntity.ok(solutionService.readCategoryAll());
     }
 
     @GetMapping("/{question_category}")
-    private ResponseEntity<List<ProblemResponseDto>> readExamByCategoryAll(@RequestParam(name = "question_category") int category){
+    public ResponseEntity<List<ProblemResponseDto>> readExamByCategoryAll(@RequestParam(name = "question_category") int category){
         return ResponseEntity.ok(solutionService.readExamByCategoryAll(category));
     }
     @PostMapping
-    private ResponseEntity<ResultResponseDto> readProblemAnswer(@RequestBody ResultRequestDto resultRequestDto){
+    public ResponseEntity<ResultResponseDto> readProblemAnswer(@RequestBody ResultRequestDto resultRequestDto){
         return ResponseEntity.ok(solutionService.readProblemAnswer(resultRequestDto));
     }
 
     @PostMapping("/result")
-    private ResponseEntity<Void> createExamResult(@RequestBody GameResultRequestDto gameResultRequestDto){
+    public ResponseEntity<Void> createExamResult(@RequestBody GameResultRequestDto gameResultRequestDto){
         try {
             solutionService.createExamResult(gameResultRequestDto);
             return ResponseEntity.ok().build();
@@ -41,14 +41,14 @@ public class SolutionController {
         }
     }
     @GetMapping("/review/{child_seq}/{date}")
-    private ResponseEntity<List<ProblemALLResponseDto>> readProblemsALL
+    public ResponseEntity<List<ProblemALLResponseDto>> readProblemsALL
             (@RequestParam(name = "child_seq") int Child_seq,
              @RequestParam(name = "date") String date){
         return ResponseEntity.ok(solutionService.readProblemsALL(Child_seq,date));
     }
 
     @PatchMapping("/review")
-    private ResponseEntity<ResultUpdateResponseDto> readProblemsALL(@RequestBody ResultUpdateRequestDto resultUpdateRequestDto){
+    public ResponseEntity<ResultUpdateResponseDto> readProblemsALL(@RequestBody ResultUpdateRequestDto resultUpdateRequestDto){
         return ResponseEntity.ok(solutionService.updateProblem(resultUpdateRequestDto));
     }
 
