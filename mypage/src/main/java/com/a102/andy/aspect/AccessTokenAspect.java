71e249e736fb9b7706f28747a240eea4ab36dbf9
유현종 <guswhd903@naver.com>
2024-03-29 10:55:33 +0900

    HOTFIX : exection location
    Former-commit-id: d83bf8a02bcf799241525831af94cfc5658ef722

--- mypage/src/main/java/com/a102/andy/aspect/AccessTokenAspect.java
+++ mypage/src/main/java/com/a102/andy/aspect/AccessTokenAspect.java
@@ -13,7 +13,7 @@ import org.springframework.web.context.request.ServletRequestAttributes;
 @Aspect
 @Component
 public class AccessTokenAspect {
-    @Around("execution(* com.a102.andy.mypage.*.*(..))")
+    @Around("execution(* com.a102.andy.mypage.controller..*.*(..))")
     public Object aroundControllerMethods(ProceedingJoinPoint joinPoint) throws Throwable {
         // 원래의 메서드를 실행합니다.
         Object proceed = joinPoint.proceed();
