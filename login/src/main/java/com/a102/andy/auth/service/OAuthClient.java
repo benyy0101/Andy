2a63fd7ffeda45f1c56f636a582a10d8c0cf63cf
유현종 <guswhd903@naver.com>
2024-04-03 17:11:53 +0900

    !HOTFIX : Delete prepared.py
    Former-commit-id: 7d01a260b555d64083bd704c713c75796742024b

--- login/src/main/java/com/a102/andy/auth/service/OAuthClient.java
+++
@@ -1,9 +0,0 @@
-package com.a102.andy.auth.service;
-
-import com.a102.andy.auth.controller.dto.OAuthAccessTokenResponse;
-import com.a102.andy.auth.controller.dto.OAuthMemberInfoResponse;
-
-public interface OAuthClient {
-    OAuthAccessTokenResponse getAccessToken(String status, String code);
-    OAuthMemberInfoResponse getMemberInfo(String accessToken);
-}
