5c5b90fc0bdaf8d174a91630eabba29e8cecf7a9
유현종 <guswhd903@naver.com>
2024-04-03 15:58:16 +0900

    !HOTFIX : Start.sh
    Former-commit-id: 85aaf73455150b1a207d954534431653e2c5482e

--- Start.sh
+++ Start.sh
@@ -5,23 +5,22 @@ set -e
 
 # Solution 서비스 빌드
 echo "Building Solution service..."
-cd Solution
+cd solution
 ./gradlew clean build -x test
 cd ..
 
 # MyPage 서비스 빌드
 echo "Building MyPage service..."
-cd MyPage
+cd mypage
 # MyPage 서비스가 Java 기반일 경우 예시입니다. 필요에 따라 변경하세요.
 ./gradlew clean build -x test
 cd ..
 
 # Login 서비스 빌드
-echo "Building Login service..."
-cd AndyLogin
-# 예를 들어 Node.js 기반일 경우 npm을 사용할 수 있습니다.
-npm install
-npm run build
+echo "Building MyPage service..."
+cd login
+# MyPage 서비스가 Java 기반일 경우 예시입니다. 필요에 따라 변경하세요.
+./gradlew clean build -x test
 cd ..
 
 echo "Starting docker-compose..."
